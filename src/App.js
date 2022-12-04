b70717be75ab67326eeeaba55ca4a7693a26b519
stevey-sy <sinsy89@hanmail.net>
2022-12-04 17:08:29 +0900

    issue # useEffect  적용

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 
-import Button from "./Button";
-import styles from "./App.module.css";
+// import Button from "./Button";
+// import styles from "./App.module.css";
+import {useState, useEffect} from "react";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onClick = () => setValue((prev) => prev+1);
+  console.log("i run all the time");
+  const onChange = (event) => setKeyword(event.target.value);
+  useEffect(()=> {
+    console.log("I run only once");
+  }, []);
+  useEffect(()=>{
+    console.log("I run when 'keyword' changes");
+  }, [keyword]);
+  useEffect(()=>{
+    console.log("I run when 'conuter' changes");
+  }, [counter]);
+
+  useEffect(()=>{
+    console.log("I run when 'keyword & counter' changes");
+  }, [keyword, counter]);
   return (
     <div >
-      <h1 className={styles.title}>Welcome back!</h1>
-      <Button text={"Continute"}/>
+      <input onChange={onChange} value={keyword} type="text" placeholder = "Search here..."/>
+      <h1>{counter}</h1>
+      <button onClick={onClick}>Click me</button>
     </div>
   );
 }
