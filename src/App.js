a9780a484542687a2d5813a8e70a004b995ab1d3
stevey-sy <sinsy89@hanmail.net>
2022-12-06 22:07:07 +0900

    issue # To Do List 만들기

--- src/App.js
+++ src/App.js
@@ -1,31 +1,28 @@
 
-// import Button from "./Button";
-// import styles from "./App.module.css";
 import {useState, useEffect} from "react";
 
-function Hello() {
-  function byeFn() {
-    console.log("bye :(");
-  }
-  
-  function hiFn() {
-    console.log("created : )");
-    return byeFn;
-  }
-
-  useEffect(hiFn, []);
-  return <h1>Hello</h1>;
-}
-
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing(prev => !prev);
-  return (
-    <div >
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}>{showing? "Hide" : "Show"}</button>
-    </div>
-  );
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if(toDo === "") {
+      return;
+    }
+    setToDo("");
+    setToDos(currentArray => [toDo, ...currentArray]);
+  };
+  console.log(toDos);
+  return <div>
+    <h1>My To Dos ({toDos.length})</h1>
+    <form>
+      <input onChange={onChange} value={toDo} type="text" placeholder="Write your to do..." />
+      <button onClick={onSubmit}>Add To Do</button>
+    </form>
+   
+  </div>
+
 }
 
 export default App;
