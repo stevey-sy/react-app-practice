99f3d967f7da228008ac2eebf2ab1461b1be69aa
stevey-sy <sinsy89@hanmail.net>
2022-12-14 21:57:49 +0900

    issue # array.map() 함수 적용

--- src/App.js
+++ src/App.js
@@ -20,7 +20,11 @@ function App() {
       <input onChange={onChange} value={toDo} type="text" placeholder="Write your to do..." />
       <button onClick={onSubmit}>Add To Do</button>
     </form>
-   
+    <hr />
+    <ul>
+      {toDos.map((item, index) => <li key={index}>{item}</li>)}
+    </ul>
+
   </div>
 
 }
